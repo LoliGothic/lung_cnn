b1ef69032d39195766daf8122e75e1df9c134b4f
daisuke.miyoshi <nana40481023@gmail.com>
2023-05-03 20:14:31 +0900

    add: 仮のデータをボクセル化する機能を追加

--- voxel_converter.py
+++ voxel_converter.py
@@ -3,10 +3,10 @@ import numpy as np
 import os
 
 # スライスされたJPEG画像が保存されたフォルダのパス
-slice_folder_path = "path/to/slice/folder"
+slice_folder_path = "./yugen"
 
 # 1つのスライス画像のサイズ
-width, height = 512, 512
+width, height = 700, 100
 
 # スライスされたJPEG画像をすべて読み込む
 slice_images = []
@@ -17,6 +17,7 @@ for filename in sorted(os.listdir(slice_folder_path)):
 
 # ボクセルデータを作成する
 depth = len(slice_images)
+# 3はR,G,Bの3つのチャンネルということ
 voxels = np.zeros((width, height, depth, 3), dtype=np.uint8)
 
 # 各スライス画像を処理し、ボクセルデータに変換する
@@ -26,5 +27,9 @@ for z, slice_image in enumerate(slice_images):
             r, g, b = slice_image.getpixel((x, y))
             voxels[x][y][z] = [r, g, b]
 
+# ToTensorが4次元には使えないので，ボクセルデータを正規化,転置する
+voxels = voxels / 255
+voxels = voxels.T
+
 # ボクセルデータをnpyファイルとして保存する
-np.save("path/to/voxels.npy", voxels)
+np.save("./yugen.npy", voxels)
