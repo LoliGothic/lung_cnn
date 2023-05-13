8a45abc56310643b5f7671671b0673a2879c8878
daisuke.miyoshi <nana40481023@gmail.com>
2023-05-13 22:41:16 +0900

    refactor: 入力チャンネルが1チャンネルで動くようにした

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ class MyDataset(torch.utils.data.Dataset):
 class MyModel(nn.Module):
     def __init__(self):
         super(MyModel, self).__init__()
-        self.conv1 = nn.Conv3d(3, 16, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv3d(1, 16, kernel_size=3, padding=1)
         self.pool1 = nn.MaxPool3d(kernel_size=2)
         self.conv2 = nn.Conv3d(16, 32, kernel_size=3, padding=1)
         self.pool2 = nn.MaxPool3d(kernel_size=2)
         self.conv3 = nn.Conv3d(32, 64, kernel_size=3, padding=1)
         self.pool3 = nn.MaxPool3d(kernel_size=2)
-        self.fc1 = nn.Linear(256*29*9, 256)  # nn.Linear（入力サイズ,出力サイズ）割り切れる値に変えてみた(poolingで出てきた値)
+        self.fc1 = nn.Linear(262144, 256)  # nn.Linear（入力サイズ,出力サイズ）割り切れる値に変えてみた(poolingで出てきた値)
         self.fc2 = nn.Linear(256, 2)  # 2値分類だから最後は2
 
     # モデルの順伝播を定義する
@@ -37,7 +37,7 @@ class MyModel(nn.Module):
         x = self.pool1(torch.relu(self.conv1(x)))
         x = self.pool2(torch.relu(self.conv2(x)))
         x = self.pool3(torch.relu(self.conv3(x)))
-        x = x.view(-1, 256*29*9)  # 割り切れる値に変えてみた(poolingで出てきた値)
+        x = x.view(-1, 262144)  # 割り切れる値に変えてみた(poolingで出てきた値)
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x
@@ -115,9 +115,9 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"device: {device}")
 
 # データセットを作成する
-data = []  # depth違くても学習できるっぽい
-tes_yugen = np.load("./yugen.npy")
-tes_soryu = np.load("./soryu.npy")
+data = []  # 入力データのdepthは合わせる必要がある
+tes_yugen = np.load("./people1.npy")
+tes_soryu = np.load("./people2.npy")
 for i in range(10):
     data.append(tes_yugen)
     data.append(tes_soryu)
