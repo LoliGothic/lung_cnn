d38ccb9634f04add6787f2efe098959735269227
ruten <nana40481023@gmail.com>
2023-06-29 20:35:55 +0900

    add: dicom画像からボクセルデータを作成

--- dicom2voxel_converter.py
+++ dicom2voxel_converter.py
@@ -8,34 +8,40 @@ all_patient_path = "./data"
 
 # 300番台の人,400番台の人...1700番台の人とループを回す
 for divide_patient_path in natsorted(os.listdir(all_patient_path)):
+
+    if divide_patient_path == "1700-":
+
         # 300番,301番...1798番,1799番とループを回す
         for patient_path in natsorted(os.listdir(path=f"{all_patient_path}/{divide_patient_path}")):
             # 患者１人ずつのボクセルデータを作りたいため，patient_pathが変わるたびに，slice_dicomsを空にする
             slice_dicoms = []
 
-                # 300番の1枚目のdicom画像,300番の2枚目のdicom画像...1799番のhoge枚目のdicom画像とループを回す
-                for filename in natsorted(os.listdir(path=f"{all_patient_path}/{divide_patient_path}/{patient_path}")):
-                    dicom = pydicom.dcmread(f"{all_patient_path}/{divide_patient_path}/{patient_path}/{filename}")
-                    slice_dicoms.append(dicom)
-                    
-                    print(filename)
-                
-                # ボクセルデータを作成する
-                depth = len(slice_dicoms)
-                # width,height,depth,channelの配列を作る
-                voxels = np.zeros((512, 512, depth, 1), dtype=np.uint8)
+            # 300番の1枚目のdicom画像,300番の2枚目のdicom画像...1799番のhoge枚目のdicom画像とループを回す
+            for filename in natsorted(os.listdir(path=f"{all_patient_path}/{divide_patient_path}/{patient_path}")):
+                dicom = pydicom.dcmread(f"{all_patient_path}/{divide_patient_path}/{patient_path}/{filename}")
+                slice_dicoms.append(dicom)
+            
+            # ボクセルデータを作成する
+            depth = len(slice_dicoms)
+            # width,height,depth,channelの配列を作る
+            voxels = np.zeros((512, 512, depth, 1), dtype=np.uint8)
 
-                # 各スライス画像を処理し、ボクセルデータに変換する
-                for z, slice_dicom in enumerate(slice_dicoms):
-                    slice_dicom_pixel = slice_dicom.pixel_array
-                    for x in range(512):
-                        for y in range(512):
+            # 各スライス画像を処理し、ボクセルデータに変換する
+            for z, slice_dicom in enumerate(slice_dicoms):
+                slice_dicom_pixel = slice_dicom.pixel_array
+                for x in range(512):
+                    for y in range(512):
+                        try:
                             voxels[x][y][z] = slice_dicom_pixel[x][y]
+                        except IndexError: # 1715だけ画像サイズが(482, 482)だから足りないとこには0を入れてあげる
+                            voxels[x][y][z] = 0
 
-                # conv3dで使いやすくするために，ボクセルデータを正規化,転置する
-                voxels = voxels / 255
-                voxels = voxels.T
+            # conv3dで使いやすくするために，ボクセルデータを正規化,転置する
+            voxels = voxels / 255
 
-                # ボクセルデータをnpyファイルとして保存する
-                np.save(f"./voxel_data/{patient_path}.npy", voxels)
-                print(patient_path)
+            # ここからリサイズとかをしていくのでまだ転置はしなくていい？
+            # voxels = voxels.T
+
+            # ボクセルデータをnpyファイルとして保存する
+            np.save(f"./voxel_data/{patient_path}.npy", voxels)
+            print(patient_path)
