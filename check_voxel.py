33dd075fd219c08d1cef7429c7300e4ca018a095
daisuke.miyoshi <nana40481023@gmail.com>
2023-07-10 13:57:45 +0900

    refactor: いろいろなチェック

--- check_voxel.py
+++ check_voxel.py
@@ -4,7 +4,7 @@ import torch
 test = np.load("./yugen.npy")
 test = torch.tensor(test)
 test = test.float()
-
+print(test.size())
 targets = []
 for i in range(10):
   targets.append(1)
@@ -12,3 +12,12 @@ for i in range(10):
 # PyTorchのクラス内ではtorch.float型が前提
 targets = torch.tensor(targets).float()
 print(targets.dtype)
+
+# # Trick to accept different input shapes
+# x = torch.rand((1, 1) + (32,32,32))
+# first_fc_in_features = 1
+# print(x.size())
+# for n in x.size()[1:]:
+#     first_fc_in_features *= n
+# x = x.view(x.size(0), -1)
+# print(x.size())
