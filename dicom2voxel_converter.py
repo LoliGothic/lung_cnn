3eff6a325f154e915c0661ee326d97ef9756d7be
daisuke.miyoshi <nana40481023@gmail.com>
2023-09-01 12:12:29 +0900

    refactor: int32に変更

--- dicom2voxel_converter.py
+++ dicom2voxel_converter.py
@@ -3,7 +3,7 @@ import pydicom
 import numpy as np
 
 # スライスされたdicom画像が保存されたフォルダのパス
-slice_folder_path = "./dicom/people2/"
+slice_folder_path = "./dicom/people1/"
 
 # スライスされたdicom画像をすべて読み込む
 slice_dicoms = []
@@ -14,7 +14,7 @@ for filename in sorted(os.listdir(slice_folder_path)):
 # ボクセルデータを作成する
 depth = len(slice_dicoms)
 # width,height,depth,channelの配列を作る
-voxels = np.zeros((512, 512, depth, 1), dtype=np.uint8)
+voxels = np.zeros((512, 512, depth, 1), dtype=np.uint32)
 
 # 各スライス画像を処理し、ボクセルデータに変換する
 for z, slice_dicom in enumerate(slice_dicoms):
@@ -23,9 +23,10 @@ for z, slice_dicom in enumerate(slice_dicoms):
         for y in range(512):
             voxels[x][y][z] = slice_dicom_pixel[x][y]
 
+print(voxels.max())
 # conv3dで使いやすくするために，ボクセルデータを正規化,転置する
-voxels = voxels / 255
+voxels = voxels / voxels.max()
 voxels = voxels.T
 print(voxels)
 # ボクセルデータをnpyファイルとして保存する
-np.save("./people2.npy", voxels)
+np.save("./people1_200.npy", voxels)
